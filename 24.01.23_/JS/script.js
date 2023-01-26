c4f66d51315e8f0d949667df3739304c1c199aa6
Kaklesha <k-korni1ov@yandex.ru>
2023-01-26 13:00:36 +0400

    first commit

---
+++ 24.01.23_/JS/script.js
@@ -0,0 +1,113 @@
+" use strict"
+
+//let number = 4.6;
+
+//console.log(number);
+/*
+console.log("string"*9);
+
+const persone= "alex";
+
+console.log(persone);
+
+const persone1= 'alex';
+
+console.log(persone1);
+
+const persone2= `alex`;
+
+console.log(persone2);
+
+const bool = true;
+
+const bool1 = false;
+
+console.log(typeof(something));
+
+*/
+
+// const obj = {
+
+//     name: "Johs",
+//     age: `25`,
+//     isMarried:`false`
+//   };
+
+// console.log(obj.name, obj.age);
+// console.log(obj[`name`]);
+
+// let arr = [ `plum.png`, `orange.jpg`,`apple.bmp` ];
+
+
+// //  arr.forEach(element => {
+// //     console.log(element);
+// // });
+
+
+
+// console.log(arr[1]);
+
+
+// const ort = {
+//     a: `2`,
+//     2: `3`
+// };
+// ort[`3`]=`313`;
+// console.log(ort[`3`]);
+
+
+// const orty = {
+//     abc: {
+//         def: {
+
+//         }
+//     }
+// }
+
+// const storeName = `DoggiHouse`;
+// storeDescription = {
+//     budget: 10000,
+//     employees: [`Raichel`,`Ivan`,`Grigory`],
+//     products: {
+//         cuts: 200,
+//         hair:3000
+//     },
+//     open: true
+// };
+
+// console.log(typeof(storeDescription[`open`]));
+// // console.log(typeof(storeDescription[`products`]));
+// const fr = confirm("are winning son");
+// const catagory = `toys`;
+
+// console.log(`https://someurl.com/${catagory}/5`);
+
+// const user =`Ivan`;
+// alert(`Help, ${Ivan}`);
+//console.log(4+ +"4");
+
+
+//1
+
+numberOfFilms = prompt(`How many are you watched movies?`,`3`);
+alert(numberOfFilms);
+
+//2 
+
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMovieDB[`count`]=numberOfFilms;
+
+dump1 =prompt(`Can you say one of last movies what watched`,`Fight club`);
+
+dump2 =prompt(`How would you rate him`,`7`);
+
+
+alert(personalMovieDB[`movies['dump1']=dump2`]);
+
